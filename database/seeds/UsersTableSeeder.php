89060bb91ff53e5b0140dc7a29bd08b2b8cc9a49
thienphu103 <thienphu103@gmail.com>
2018-01-16 17:23:11 +0700

    faker user database

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+  $faker = Faker\Factory::create();
+
+    for($i = 0; $i < 1000; $i++) {
+        App\User::create([
+            'name' => $faker->userName,
+            
+            'email' => $faker->email,
+	    'password'=> $faker->password,
+        ]);
+    }
+
+}
+
+}
